26da36b957d4b987314fdb76cd651c25e6fc4bd6
clayton easterbrooks <claytoneast@gmail.com>
2015-11-18 13:28:43 -0800

    find nearby colors to highlight

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -2,37 +2,45 @@ console.log('init')
 $(document).ready(function(){
   $('a').hover(function(){
     space = $(this)
-    space.toggleClass('selected')
-    thisColor = getColor(space);
-    thisCoords = getCoords(space);
-    sameSameNeighbors = allSameNeighbors(thisCoords, thisColor);
-    // neighbors = findAllNeighbors(getCoords(space))
-    // sameNeighbors = colorNeighbors(neighbors, color);
+    sameNeighbors = arrayCheckLayer(space);
+    sameNeighbors.forEach(function(neighbor) {
+      neighbor = neighbor.split(",");
+      $("#coordX" + neighbor[0] + "coordY" + neighbor[1]).toggleClass("selected");
+    })
+
   });
 });
 
-
-  var connectedNeighbors = [];
-function allSameNeighbors(coords, color) {
-  neighbors = findAllNeighbors(coords);
-  sameNeighbors = colorNeighbors(neighbors, color);
-  // debugger;
-  sameNeighbors.forEach(function(neighbor) {
-    connectedNeighbors.push(neighbor)
-    neighborCoords = getCoords(neighbor);
-    neighborColor = getColor(neighbor)
-    allSameNeighbors(neighborCoords, neighborColor)
-  });
-  console.log(connectedNeighbors)
-  return connectedNeighbors;
-  // debugger;
-  // going to take a space, and call find its neighbors.
-  // then going to find all its same-colored neighbors
-  // then going to take each of these same colored neighbors
-  // and pass them into itself to find all of its same-colored neighbors
-
+function arrayCheckLayer(space) {
+  masterCheckArray = [];
+  adjSpaces = getBlock([space]);
+  function getBlock(spaces) {
+    colorBlock = [];
+    spaces.forEach(function(space) {
+      // if space isn't already in the masterCheckArray, then push it there, then check it. else nothing.
+      if (masterCheckArray.indexOf(flatSpace(space)) <= -1) { // if not in there do this
+        masterCheckArray.push(flatSpace(space)); // push into check array
+        colorBlock.push(space);
+        neighbors = findAllNeighbors(space); // returns coordinates, not UI objects
+        sameNeighbors = colorNeighbors(neighbors, getColor(space)); // returns an array
+        if (sameNeighbors.length > 0) { // if this array has something in it then send it to getBlock
+          getBlock(sameNeighbors);
+        } else {
+          return
+        }
+      } else {
+        return
+      }
+    });
+    return colorBlock;
+  }
+  console.log(masterCheckArray)
+  return masterCheckArray
 }
 
+function flatSpace(space) {
+  return getCoords(space).toString();
+}
 
 var colorNeighbors = function (neighbors, color) {
   var sameColorNeighbors = []
@@ -45,8 +53,9 @@ var colorNeighbors = function (neighbors, color) {
   return sameColorNeighbors
 }
 
-var findAllNeighbors = function (coords) {
+var findAllNeighbors = function (space) {
   var neighbors = [];
+  coords = getCoords(space);
   if (coords[1]-1 >= 0) {
     nAbove = ['above', coords[0], coords[1]-1]
     neighbors.push(nAbove)
@@ -71,7 +80,6 @@ var getCoords = function(element) {
   coordY = parseInt($(element).attr('data-coordY'));
   return [coordX, coordY];
 }
-
 var getColor = function(element) {
   color = $(element).attr('data-color');
   return color;
