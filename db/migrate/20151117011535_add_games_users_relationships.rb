c2040dd47f45b621717d5dbb2677495168d50470
clayton easterbrooks <claytoneast@gmail.com>
2015-12-09 10:57:46 -0800

    error

--- db/migrate/20151117011535_add_games_users_relationships.rb
+++ db/migrate/20151117011535_add_games_users_relationships.rb
@@ -4,14 +4,10 @@ class AddGamesUsersRelationships < ActiveRecord::Migration
       t.belongs_to :game, index: true
       t.belongs_to :user, index: true
     end
-    
+
     create_table :games do |t|
       t.column :gamestate, :json
       t.column :active, :boolean
     end
-
-    add_column :games, :gamestate, :json
-    add_column :games, :active, :boolean
-
   end
 end
